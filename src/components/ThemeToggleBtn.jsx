b450efcb3740b4bc87be52a41692727cfa2844b1
Mohit Pawar <mohitpawar1109@gmail.com>
2026-02-13 18:18:14 +0530

    "Creating Navbar and Toggle Theme"

---
+++ src/components/ThemeToggleBtn.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react'
+import assets from '../assets/assets'
+
+const ThemeToggleBtn = ({ theme, setTheme }) => {
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark')
+    }
+    else {
+      document.documentElement.classList.remove('dark')
+    }
+
+    localStorage.setItem('theme', theme)
+
+
+  }, [theme])
+
+
+  return (
+    <>
+      <button>
+        {theme === 'dark' ? (
+          <img onClick={() => setTheme('light')} src={assets.sun_icon} alt="toggle theme" className='size-8.5 p-1.5 border border-gray-500 rounded-full' />
+        ) : (<img onClick={() => setTheme('dark')} src={assets.moon_icon} alt="toggle theme" className='size-8.5 p-1.5 border border-gray-500 rounded-full' />)}
+      </button>
+    </>
+  )
+}
+
+export default ThemeToggleBtn
