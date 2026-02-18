76bae9d11f0559444a2133344ecff8e96df20e7c
Mohit Pawar <mohitpawar1109@gmail.com>
2026-02-18 14:42:53 +0530

    pre form authentication

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Trustedby from './components/Trustedby'
 import Services from './components/Services'
+import OurWork from './components/OurWork'
+import Teams from './components/Teams'
+import ContactUs from './components/ContactUs'
 
 const App = () => {
   const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light' )
@@ -12,6 +15,9 @@ const App = () => {
       <Hero/>
       <Trustedby/>
       <Services/>
+      <OurWork/>
+      <Teams/>
+      <ContactUs/>
     </div>
   )
 }
---
+++ src/components/ContactUs.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Title from './Title'
+import assets from '../assets/assets'
+
+const ContactUs = () => {
+    return (
+        <div id='contact-us' className='flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
+            <Title title='Reach out to us' desc='Get in touch with us for any inquiries or support.' />
+
+            <form className='grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full' >
+                <div>
+                    <p className='mb-2 text-sm font-medium'>Your name</p>
+                    <div className='flex pl-3 rounded-lg border border-gray-300 dark:border-gray-600'>
+                        <img src={assets.person_icon} alt="person-icon" />
+                        <input type="text" required placeholder='Enter your name' className='w-full p-3 text-sm outline-none' />
+                    </div>
+                </div>
+
+                <div>
+                    <p className='mb-2 text-sm font-medium'>Email id</p>
+                    <div className='flex pl-3 rounded-lg border border-gray-300 dark:border-gray-600'>
+                        <img src={assets.email_icon} alt="email-icon" />
+                        <input type="email" required placeholder='Enter your email' className='w-full p-3 text-sm outline-none' />
+                    </div>
+                </div>
+
+                <div className='sm:col-span-2'>
+                    <p className='mb-2 text-sm font-medium'>Your message</p>
+                    <textarea rows={8} placeholder='Enter your message' className='w-full p-3 text-sm outline-none rounded-lg border border-gray-300 dark:border-gray-600' />
+                </div>
+
+                <button type='submit' className='w-max flex gap-2 bg-primary text-white text-sm px-10 py-3 rounded-full cursor-pointer hover:scale-103 transition-all'>
+                    Submit <img src={assets.arrow_icon} alt="submit-arrow" className='w-4' />
+                </button>
+            </form >
+
+        </div>
+    )
+}
+
+export default ContactUs
